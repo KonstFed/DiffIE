b4d0af676f7676c4ac151850984710937f0ef637
KonstFed <konstantin_fedorov_03@mail.ru>
2026-01-18 21:49:43 +0800

    freeze embeddings

--- src/diffopenie/models/label_mapper.py
+++ src/diffopenie/models/label_mapper.py
@@ -10,6 +10,7 @@ class LabelMapper(nn.Module):
         super().__init__()
         self.num_classes = num_classes
         self.embs = nn.Embedding(num_classes, embedding_dim=embedding_dim)
+        self.embs.weight.requires_grad = False  # Freeze embedding weights
         self.embedding_dim = embedding_dim
 
     def forward(self, labels: torch.LongTensor) -> torch.FloatTensor:
