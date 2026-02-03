0b45b311fb39c021b6eec956a7960b172d7525c4
KonstFed <konstantin_fedorov_03@mail.ru>
2026-02-03 17:08:16 +0800

    pretty print util

--- src/diffopenie/utils.py
+++ src/diffopenie/utils.py
@@ -27,3 +27,52 @@ def save_config(model: BaseModel, cfg_p: Path | str) -> None:
     # Write the dictionary to a YAML file
     with cfg_p.open("w") as f:
         yaml.safe_dump(model_dict, f, default_flow_style=False, sort_keys=False)
+
+
+# ------------------------------ Pretty print functions ------------------------------
+
+RELATION_COLOR = "\033[95m"
+SUBJECT_COLOR = "\033[96m"
+OBJECT_COLOR = "\033[92m"
+RESET = "\033[0m"
+
+
+def hprint(
+    words: list[str],
+    subject_ind: list[int],
+    object_ind: list[int],
+    relation_ind: list[int],
+    legend: bool = False,
+) -> None:
+    """Colourful print for triplets given by word indices"""
+
+    if legend:
+        print(f"{SUBJECT_COLOR}Subject{RESET}: {SUBJECT_COLOR}A0{RESET}")
+        print(f"{RELATION_COLOR}Relation{RESET}: {RELATION_COLOR}P{RESET}")
+        print(f"{OBJECT_COLOR}Object{RESET}: {OBJECT_COLOR}A1{RESET}")
+
+    for i, word in enumerate(words):
+        if i in subject_ind:
+            print(SUBJECT_COLOR + word + RESET, end=" ")
+        elif i in object_ind:
+            print(OBJECT_COLOR + word + RESET, end=" ")
+        elif i in relation_ind:
+            print(RELATION_COLOR + word + RESET, end=" ")
+        else:
+            print(word, end=" ")
+    print()
+
+
+def hprint_s(
+    words: list[str],
+    subject_span: tuple[int, int],
+    object_span: tuple[int, int],
+    relation_span: tuple[int, int],
+    legend: bool = False,
+) -> None:
+    """Colourful print for subject, object, and relation spans"""
+    subject_ind = [i for i in range(subject_span[0], subject_span[1] + 1)]
+    object_ind = [i for i in range(object_span[0], object_span[1] + 1)]
+    relation_ind = [i for i in range(relation_span[0], relation_span[1] + 1)]
+
+    hprint(words, subject_ind, object_ind, relation_ind, legend)
