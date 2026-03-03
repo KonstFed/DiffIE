31cefa89e9314f3145053c35da892c9b9c60d191
KonstFed <k.fedorov@innopolis.university>
2026-03-03 16:11:45 +0800

    full exp

--- src/diffopenie/data/imojie.py
+++ src/diffopenie/data/imojie.py
@@ -142,7 +142,7 @@ class SequenceImojieDataset(Dataset):
         self.path = str(path)
         self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
         self.min_success_pct = min_success_pct
-        self._df = _load_imojie_tsv(path).iloc[:100]
+        self._df = _load_imojie_tsv(path)
         if min_success_pct is not None:
             self._filter_by_success(min_success_pct)
 
